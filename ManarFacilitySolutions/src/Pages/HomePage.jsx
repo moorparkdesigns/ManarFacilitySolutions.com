767ca395c7f7dc40acc383973f63077cd2ff610e
Nelani Maluka <malukanelani@gmail.com>
2025-06-30 14:35:58 +0200

    added home page

---
+++ ManarFacilitySolutions/src/Pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import HeroSection from "../Components/HeroSection/HeroSection";
+import MissionSection from "../Components/MissionSection/MissionSection";
+import ServicesSection from "../Components/ServicesSection/ServicesSection";
+import CtaSection from "../Components/CtaSection/CtaSection";
+
+function HomePage() {
+  return (
+    <>
+      <HeroSection />
+      <MissionSection />
+      <ServicesSection />
+      <CtaSection />
+    </>
+  );
+}
+
+export default HomePage;
