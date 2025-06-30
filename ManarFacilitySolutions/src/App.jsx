2774cc08c35a97e7852dde14e90d8fb6b384d000
Nelani Maluka <malukanelani@gmail.com>
2025-06-30 14:36:14 +0200

    updated layout

--- ManarFacilitySolutions/src/App.jsx
+++ ManarFacilitySolutions/src/App.jsx
@@ -1,11 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
+import HomePage from "./Pages/HomePage";
 
-function App() {
+function Layout({ children }) {
   return (
     <>
       <Navbar />
+      <div className="content">{children}</div>
     </>
+  );
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <HomePage />
+          </Layout>
+        }
+      />
+      <Route path="/services" element={<Layout>{/*<Services />*/}</Layout>} />
+      <Route path="/about" element={<Layout>{/*<About />*/}</Layout>} />
+      <Route path="/contact" element={<Layout>{/* <Contact />*/}</Layout>} />
+    </Routes>
   );
 }
 
