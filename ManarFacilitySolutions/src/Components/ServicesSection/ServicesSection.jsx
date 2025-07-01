ca3aa016fba4fa0566f520459b326a5fedb07d02
Nelani Maluka <malukanelani@gmail.com>
2025-07-01 15:32:24 +0200

    chore: completed the services section

--- ManarFacilitySolutions/src/Components/ServicesSection/ServicesSection.jsx
+++ ManarFacilitySolutions/src/Components/ServicesSection/ServicesSection.jsx
@@ -1,26 +1,48 @@
+import { Link } from "react-router-dom";
 import "./ServicesSection.css";
 
 function ServicesSection() {
+  const services = [
+    {
+      title: "Commercial Cleaning",
+      icon: "/Images/Icons/Icon1.png",
+      background: "/Images/Back-ground-images/5.jpg",
+      link: "/Services",
+    },
+    {
+      title: "Residential Cleaning",
+      icon: "/Images/Icons/Icon2.png",
+      background: "/Images/Back-ground-images/6.jpg",
+      link: "/Services",
+    },
+    {
+      title: "Specialized Cleaning",
+      icon: "/Images/Icons/Icon3.png",
+      background: "/Images/Back-ground-images/7.jpg",
+      link: "/Services",
+    },
+  ];
+
   return (
     <div className="ServicesSection">
-      <div className="container">
+      <div className="container-2">
         <h1>Services we offer</h1>
         <div className="content">
-          <div
-            style={{
-              backgroundImage: `url("/Images/Back-ground-images/5.jpg")`,
-            }}
-          ></div>
-          <div
-            style={{
-              backgroundImage: `url("/Images/Back-ground-images/6.jpg")`,
-            }}
-          ></div>
-          <div
-            style={{
-              backgroundImage: `url("/Images/Back-ground-images/7.jpg")`,
-            }}
-          ></div>
+          {services.map((service, index) => (
+            <Link to={service.link} key={index} className="card-link">
+              <div
+                style={{
+                  backgroundImage: `url(${service.background})`,
+                }}
+              >
+                <div className="card-content">
+                  <img src={service.icon} alt="Icon" />
+                  <h1>{service.title}</h1>
+                  <span>Click to learn more</span>
+                </div>
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
     </div>
