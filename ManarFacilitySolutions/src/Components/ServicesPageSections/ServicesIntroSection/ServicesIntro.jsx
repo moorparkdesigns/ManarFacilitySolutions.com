babc4d52a992f090b3a04717047dd00aebd3ec80
Sairam Soundararajan <sai20879@gmail.com>
2025-07-07 12:10:06 -0400

    added h1 and p tag to services intro

--- ManarFacilitySolutions/src/Components/ServicesPageSections/ServicesIntroSection/ServicesIntro.jsx
+++ ManarFacilitySolutions/src/Components/ServicesPageSections/ServicesIntroSection/ServicesIntro.jsx
@@ -1,7 +1,10 @@
 import "./ServicesIntro.css";
 
 function ServicesIntroSection() {
-  return <div className="services-intro"> </div>;
+  return <div className="services-intro">
+    <h1>Our Services</h1>
+    <p>At Manar Facility Solutions, we offer a wide variety of services that range from commercial and residential to specialized cleaning.</p>
+  </div>;
 }
 
 export default ServicesIntroSection;
