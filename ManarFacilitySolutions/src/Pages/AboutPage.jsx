b448340282974e6c2590c9515a68e65d1ccdf2c7
Nelani Maluka <malukanelani@gmail.com>
2025-07-11 15:54:26 +0200

    added react hamlet for seo

--- ManarFacilitySolutions/src/Pages/AboutPage.jsx
+++ ManarFacilitySolutions/src/Pages/AboutPage.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet-async";
 import { Link } from "react-router-dom";
 import AboutIntro from "../Components/AboutPageSections/AboutIntro/AboutIntro";
 import MissionSection from "../Components/HomePageSections/MissionSection/MissionSection";
@@ -11,8 +12,8 @@ const aboutDescription = (
     team who has been in the custodial field for over 20 years.
     <br />
     <br />
-    Manar Facility Solutions works with with an incredible team of hard-working
-    home and office cleaners who simply know how to clean– and do it exceedingly
+    Manar Facility Solutions works with an incredible team of hard-working home
+    and office cleaners who simply know how to clean– and do it exceedingly
     well.
     <br />
     <br />
@@ -28,6 +29,89 @@ const img2 = "/Images/Back-ground-images/9.jpg";
 function AboutPage() {
   return (
     <>
+      <Helmet key="about-page">
+        {/* Primary Meta */}
+        <title>About Us | Manar Facility Solutions</title>
+        <meta
+          name="description"
+          content="Founded by a husband and wife team with over 20 years in the custodial field, Manar Facility Solutions offers top-tier residential and commercial cleaning services in Alachua County, Gainesville, Newberry, Archer, High Springs, and surrounding areas."
+        />
+        <meta
+          name="keywords"
+          content="Manar Facility Solutions, Cleaning Services, Alachua, Gainesville, Newberry, High Springs, Archer, FL Cleaning Company, Custodial Services, Residential Cleaning, Office Cleaning"
+        />
+        <link rel="canonical" href="https://manarfacilitysolutions.com/About" />
+
+        {/* Open Graph / Facebook */}
+        <meta
+          property="og:title"
+          content="About Us | Manar Facility Solutions"
+        />
+        <meta
+          property="og:description"
+          content="Professional cleaning services in Gainesville, Alachua, Newberry, Archer, and surrounding FL cities. Learn more about our story and team."
+        />
+        <meta property="og:image" content={img2} />
+        <meta
+          property="og:url"
+          content="https://manarfacilitysolutions.com/About"
+        />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="About Us | Manar Facility Solutions"
+        />
+        <meta
+          name="twitter:description"
+          content="Trusted cleaning experts in Alachua County and nearby cities. Manar Facility Solutions delivers professional home and office cleaning services."
+        />
+        <meta name="twitter:image" content={img2} />
+
+        {/* Preload important image */}
+        <link rel="preload" as="image" href={img2} />
+
+        {/* Schema JSON-LD */}
+        <script type="application/ld+json">
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "LocalBusiness",
+            name: "Manar Facility Solutions",
+            image: "https://www.manarfacilitysolutions.com" + img2,
+            address: {
+              "@type": "PostalAddress",
+              streetAddress: "5145 SW 75th St #348",
+              addressLocality: "Gainesville",
+              addressRegion: "FL",
+              postalCode: "32608",
+              addressCountry: "US",
+            },
+            telephone: "+1-352-966-2627",
+            email: "info@manarhomesevices.com",
+            url: "https://www.manarfacilitysolutions.com",
+            description:
+              "Manar Facility Solutions provides residential and commercial cleaning services in Gainesville, Alachua, Newberry, Archer, High Springs, and other areas in North Central Florida.",
+            areaServed: [
+              "Gainesville",
+              "Alachua",
+              "Newberry",
+              "High Springs",
+              "Archer",
+              "Jonesville",
+              "Tioga",
+              "Micanopy",
+            ],
+            priceRange: "$$",
+            founder: {
+              "@type": "Person",
+              name: "Husband and Wife Team",
+            },
+          })}
+        </script>
+      </Helmet>
+
       <AboutIntro />
       <MissionSection
         title={aboutTitle}
