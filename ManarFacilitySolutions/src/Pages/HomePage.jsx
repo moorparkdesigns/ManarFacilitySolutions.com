6add77786f2e4467bfb8a5567c23e3d2e1b479c2
Nelani Maluka <malukanelani@gmail.com>
2025-07-11 15:05:25 +0200

    refactored mission section to be dynamic

--- ManarFacilitySolutions/src/Pages/HomePage.jsx
+++ ManarFacilitySolutions/src/Pages/HomePage.jsx
@@ -5,10 +5,36 @@ import ReviewsSection from "../Components/HomePageSections/ReviewsSection/Review
 import CtaSection from "../Components/GlobalSection/CtaSection/CtaSection";
 
 function HomePage() {
+  const missionTitle = "Our mission";
+  const missionDescription = (
+    <>
+      At Manar Facility Solutions, we offer high-quality cleaning services while
+      providing a strong foundation of trust and respect for our customers.
+      <br />
+      <br />
+      We have experience working with every home style, from apartments to
+      multi-family homes. Our core value is to provide you with 5-star services
+      and ensure that your every need is taken care of.
+      <br />
+      <br />
+      Schedule a cleaning for your personal or professional space today to
+      experience our 5-star services.
+    </>
+  );
+
+  const img1 = "/Images/Back-ground-images/(4)-2.png";
+  const img2 = "/Images/Back-ground-images/4.jpg";
+
   return (
     <>
       <HeroSection />
-      <MissionSection />
+      <MissionSection
+        title={missionTitle}
+        description={missionDescription}
+        button={true}
+        img1={img1}
+        img2={img2}
+      />
       <ServicesSection />
       <ReviewsSection />
       <CtaSection />
