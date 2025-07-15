4ae8c4f151d5dc38a788395a554a8ee565fe4f31
Nelani Maluka <malukanelani@gmail.com>
2025-07-15 15:44:03 +0200

    refactored the mission section to include images next to the h1 tag

--- ManarFacilitySolutions/src/Components/HomePageSections/MissionSection/MissionSection.jsx
+++ ManarFacilitySolutions/src/Components/HomePageSections/MissionSection/MissionSection.jsx
@@ -1,27 +1,68 @@
 import { Link } from "react-router-dom";
 import "./MissionSection.css";
 
-function MissionSection({ title, description, button, img1, img2 }) {
+function MissionSection({
+  img0,
+  title,
+  description,
+  button,
+  img1,
+  img2,
+  style,
+}) {
+  const isStyle2 = style === "style-2";
   return (
-    <div className="our-mission container-2">
-      <div>
-        <h1>{title}</h1>
-        <p>{description}</p>
-        {button && (
-          <Link to="/About">
-            <button>About Us</button>
-          </Link>
-        )}
-      </div>
-      <div className="img">
-        <picture>
-          {/* Tablet image */}
-          <source media="(max-width: 991px)" srcSet={img1} />
-
-          {/* Default (desktop) image */}
-          <img src={img2} alt="Our mission image" />
-        </picture>
-      </div>
+    <div className={`our-mission container-2 ${isStyle2 ? "style-2" : ""}`}>
+      {/* If style-2, reverse the order: image first, then text */}
+      {isStyle2 ? (
+        <>
+          <div className="img">
+            <picture>
+              <source
+                media="(min-width: 577px) and (max-width: 991px)"
+                srcSet={img1}
+              />
+              <img src={img2} alt="Our mission image" />
+            </picture>
+          </div>
+          <div className="text">
+            <div className="img-text">
+              {img0 && <img src={img0} alt="icon" />}
+              <h1>{title}</h1>
+            </div>
+            <p>{description}</p>
+            {button && (
+              <Link to="/About">
+                <button>About Us</button>
+              </Link>
+            )}
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="text">
+            <div className="img-text">
+              {img0 && <img src={img0} alt="icon" />}
+              <h1>{title}</h1>
+            </div>
+            <p>{description}</p>
+            {button && (
+              <Link to="/About">
+                <button>About Us</button>
+              </Link>
+            )}
+          </div>
+          <div className="img">
+            <picture>
+              <source
+                media="(min-width: 577px) and (max-width: 991px)"
+                srcSet={img1}
+              />
+              <img src={img2} alt="Our mission image" />
+            </picture>
+          </div>
+        </>
+      )}
     </div>
   );
 }
